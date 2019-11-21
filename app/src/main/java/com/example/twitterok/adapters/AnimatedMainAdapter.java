077998006847290172b66e3a9bf0f2d6324a8cdb
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-11-21 15:34:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/twitterok/adapters/AnimatedMainAdapter.java
@@ -0,0 +1,22 @@
+package com.example.twitterok.adapters;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import android.view.animation.AnimationUtils;
+
+import com.example.twitterok.R;
+
+public class AnimatedMainAdapter extends MainAdapter{
+
+    public AnimatedMainAdapter(@LayoutRes int resId) {
+        super(resId);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseAdapter.CommonViewHolder holder, int i) {
+        super.onBindViewHolder(holder, i);
+        holder.
+                itemView.
+                setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.news_item_anim));
+    }
+}
---
+++ app/src/main/java/com/example/twitterok/adapters/BaseAdapter.java
@@ -0,0 +1,87 @@
+package com.example.twitterok.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.databinding.library.baseAdapters.BR;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseAdapter<T> extends RecyclerView.Adapter<BaseAdapter.CommonViewHolder>{
+
+    @LayoutRes protected int header;
+    protected List<T> data;
+    private final T EMPTY_OBJ_FOR_HEADER_POSITION = null;
+
+
+    public BaseAdapter(){
+        data = new ArrayList<>();
+        data.add(EMPTY_OBJ_FOR_HEADER_POSITION);
+    }
+
+    @NonNull
+    @Override
+    public CommonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ViewDataBinding binding = DataBindingUtil.inflate(inflater,viewType,parent,false);
+        return new CommonViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseAdapter.CommonViewHolder holder, int position) {
+        Object obj = getObjectForPosition(position);
+        holder.bind(obj);
+    }
+
+
+    protected abstract Object getObjectForPosition(int position);
+    protected abstract int getLayoutIdForPosition(int position);
+
+    public void setHeader(@LayoutRes int headerId){
+        this.header = headerId;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return getLayoutIdForPosition(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public void updateData(@Nullable List<T> data){
+        if(data == null || data.isEmpty()){
+            this.data.clear();
+        }else {
+            this.data.addAll(data);
+        }
+        notifyDataSetChanged();
+    }
+
+    public class CommonViewHolder extends RecyclerView.ViewHolder{
+        ViewDataBinding binding;
+
+        public CommonViewHolder(@NonNull ViewDataBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(Object obj){
+            if(obj != null) {
+                binding.setVariable(BR.viewModel, obj);
+                binding.executePendingBindings();
+            }
+        }
+    }
+}
+
