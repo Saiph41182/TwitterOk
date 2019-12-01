59d15f217dee18f760c1602fe485dc741a7086b9
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-01 16:41:25 +0300

    DataViewModel which was providing binding recyclerView with data are delete

--- app/src/main/java/com/example/twitterok/view/MainFragment.java
+++ app/src/main/java/com/example/twitterok/view/MainFragment.java
@@ -1,26 +1,27 @@
 package com.example.twitterok.view;
 
 
-import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
+import androidx.databinding.library.baseAdapters.BR;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.twitterok.App;
 import com.example.twitterok.R;
-import com.example.twitterok.adapters.AnimatedMainAdapter;
-import com.example.twitterok.adapters.MainAdapter;
-import com.example.twitterok.databinding.FragmentMainBinding;
 
-import com.example.twitterok.viewmodel.DataViewModel;
+import com.example.twitterok.repository.internet.TwitterApiProvider;
+import com.example.twitterok.view.adapters.AnimatedMainAdapter;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,36 +29,32 @@ import com.example.twitterok.viewmodel.DataViewModel;
 public class MainFragment extends Fragment {
     public static final String TAG = "MainFragment";
 
-    private DataViewModel dataViewModel = new DataViewModel();
-
     public MainFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FragmentMainBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false);
-        View view = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
         initRecyclerView(view);
-        binding.setViewModel(dataViewModel);
         return view;
     }
     private void initRecyclerView(View view){
-        RecyclerView recyclerView = view.findViewById(R.id.main_recycler_view);
+        RecyclerView recyclerView = view.findViewById(R.id.timeline_recycler_view);
+        AnimatedMainAdapter adapter = new AnimatedMainAdapter(R.layout.item_tweet_view);
+        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        downloadData(adapter);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-            dataViewModel.setUp();
-    }
+    public void downloadData(AnimatedMainAdapter mainAdapter){
+        TwitterApiProvider twitterApiClient = new TwitterApiProvider(App.getSession());
+        Disposable disposable = twitterApiClient
+                .getHomeTimeline(200)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mainAdapter::updateData);
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        dataViewModel.tearDown();
     }
-
 
 }
