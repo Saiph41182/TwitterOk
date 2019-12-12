cadb16e679fc24a864d5b940b0802329a0cce8d8
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-12 17:05:13 +0300

    Data loading moved to repository

--- app/src/main/java/com/example/twitterok/view/MainFragment.java
+++ app/src/main/java/com/example/twitterok/view/MainFragment.java
@@ -4,7 +4,6 @@ package com.example.twitterok.view;
 import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
-import androidx.databinding.library.baseAdapters.BR;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,45 +15,49 @@ import android.view.ViewGroup;
 import com.example.twitterok.App;
 import com.example.twitterok.R;
 
-import com.example.twitterok.repository.internet.TwitterApiProvider;
-import com.example.twitterok.view.adapters.AnimatedMainAdapter;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import com.example.twitterok.databinding.FragmentMainBinding;
+import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
+import com.example.twitterok.databinding.viewmodel.TimelineViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
-    public static final String TAG = "MainFragment";
+
+    private TimelineViewModel viewModel;
 
     public MainFragment() {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        if (viewModel != null){
+            viewModel.setUp();
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        FragmentMainBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false);
+        View view = binding.getRoot();
         initRecyclerView(view);
+        viewModel = new TimelineViewModel();
+        binding.setViewModel(viewModel);
+        binding.setOwnerVM(new OwnerViewModel(App.getOwner()));
         return view;
     }
     private void initRecyclerView(View view){
         RecyclerView recyclerView = view.findViewById(R.id.timeline_recycler_view);
-        AnimatedMainAdapter adapter = new AnimatedMainAdapter(R.layout.item_tweet_view);
-        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-        downloadData(adapter);
     }
 
-    public void downloadData(AnimatedMainAdapter mainAdapter){
-        TwitterApiProvider twitterApiClient = new TwitterApiProvider(App.getSession());
-        Disposable disposable = twitterApiClient
-                .getHomeTimeline(200)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(mainAdapter::updateData);
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(viewModel != null){
+            viewModel.tearDown();
+        }
     }
-
 }
