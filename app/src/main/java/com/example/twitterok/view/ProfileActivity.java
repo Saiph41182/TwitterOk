74399ee8888fcac1c900991b0356620ed0b82660
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-12 16:34:52 +0300

    Profile page support data binding. Profile page has expanding toolbar.

--- app/src/main/java/com/example/twitterok/view/ProfileActivity.java
+++ app/src/main/java/com/example/twitterok/view/ProfileActivity.java
@@ -2,11 +2,22 @@ package com.example.twitterok.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.twitterok.App;
 import com.example.twitterok.R;
+import com.example.twitterok.databinding.ActivityProfileBinding;
+import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
+import com.example.twitterok.databinding.viewmodel.ProfileTimelineViewModel;
+import com.example.twitterok.databinding.viewmodel.TimelineViewModel;
 import com.example.twitterok.view.adapters.MainAdapter;
 import com.example.twitterok.repository.internet.TwitterApiProvider;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -15,30 +26,50 @@ import io.reactivex.schedulers.Schedulers;
 
 public class ProfileActivity extends AppCompatActivity {
 
+
+    private ProfileTimelineViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        initRecyclerView();
+        ActivityProfileBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_profile);
+        initRecyclerView(binding.getRoot());
+        binding.setOwnerVM(new OwnerViewModel(App.getOwner()));
+        viewModel = new ProfileTimelineViewModel();
+        binding.setViewModel(viewModel);
+        binding.profileBaseInfo.setOnClickListener(view -> {
+            LinearLayout expendableView = binding.expendableView;
+            if(expendableView.getVisibility() == View.GONE){
+                TransitionManager.beginDelayedTransition(binding.profileAppBar,new AutoTransition());
+                expendableView.setVisibility(View.VISIBLE);
+                binding.profileArrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+            }else{
+                TransitionManager.beginDelayedTransition(binding.profileAppBar,new AutoTransition());
+                expendableView.setVisibility(View.GONE);
+                binding.profileArrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+            }
+        });
     }
 
-    private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.user_info_tweet_list);
-        MainAdapter adapter = new MainAdapter(R.layout.item_tweet_view);
-        adapter.setHeader(R.layout.header_item_profile);
-        recyclerView.setAdapter(adapter);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(viewModel != null){
+            viewModel.setUp();
+        }
+    }
+
+    private void initRecyclerView(View view){
+        RecyclerView recyclerView = view.findViewById(R.id.user_info_tweet_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-        downloadData(adapter);
+
     }
 
-    private void downloadData(MainAdapter adapter) {
-        TwitterApiProvider twitterApiClient = new TwitterApiProvider(App.getSession());
-        Disposable disposable = twitterApiClient
-                .getUserTimeline(200,App.getOwner().getScreenName())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(data -> adapter.updateData(data));
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(viewModel!=null){
+            viewModel.tearDown();
+        }
     }
-
-
 }
