e32b4f77c8f2fc1ae254b4ef3d1a8703acfad993
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-12 16:31:02 +0300

    Added view model. Added IME action listener.

--- app/src/main/java/com/example/twitterok/view/SearchFragment.java
+++ app/src/main/java/com/example/twitterok/view/SearchFragment.java
@@ -1,18 +1,29 @@
 package com.example.twitterok.view;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.twitterok.App;
 import com.example.twitterok.R;
+import com.example.twitterok.databinding.FragmentSearchBinding;
+import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
+import com.example.twitterok.databinding.viewmodel.SearchUsersViewModel;
 import com.example.twitterok.view.adapters.SearchAdapter;
 import com.example.twitterok.repository.internet.TwitterApiProvider;
 
@@ -25,34 +36,61 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class SearchFragment extends Fragment {
 
+    private SearchUsersViewModel viewModel;
 
     public SearchFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onStart() {
+        super.onStart();
+        if(viewModel != null){
+            viewModel.setUp();
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        FragmentSearchBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_search,container,false);
+        binding.etHeaderItemSearch.setOnEditorActionListener(onEditorActionListener);
+        View view = binding.getRoot();
         initRecyclerView(view);
+        initViewBinding(binding);
         return view;
+    }
+
+    private void initViewBinding(FragmentSearchBinding binding) {
+        if(viewModel == null){
+            viewModel = new SearchUsersViewModel();
+        }
+        binding.setViewModel(viewModel);
+        binding.setOwnerVM(new OwnerViewModel(App.getOwner()));
+        binding.executePendingBindings();
     }
 
     private void initRecyclerView(View view){
         RecyclerView recyclerView = view.findViewById(R.id.users_list);
-        SearchAdapter searchAdapter = new SearchAdapter(R.layout.item_user);
-        searchAdapter.setHeader(R.layout.header_item_user_search);
-        recyclerView.setAdapter(searchAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-        downloadUsers(searchAdapter);
     }
 
-    private void downloadUsers(SearchAdapter searchAdapter){
-        TwitterApiProvider twitterApiClient = new TwitterApiProvider(App.getSession());
-        Disposable disposable = twitterApiClient
-                .getSearch("a")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(searchAdapter::updateData);
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(viewModel != null){
+            viewModel.tearDown();
+        }
     }
+
+    private TextView.OnEditorActionListener onEditorActionListener = (v, actionId, event) -> {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            viewModel.setUp();
+            InputMethodManager inputMethodManager = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(),0);
+            return true;
+        }
+        return false;
+    };
+
 }
