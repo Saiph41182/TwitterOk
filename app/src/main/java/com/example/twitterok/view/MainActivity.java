c0492fd6857e3e92e87c31715baa6c750ce183a9
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-12 17:07:39 +0300

    Bottom app bar changed to Bottom navigation view. Bottom Navigation drawer changed to Navigation view.

--- app/src/main/java/com/example/twitterok/view/MainActivity.java
+++ app/src/main/java/com/example/twitterok/view/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.twitterok.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.bottomappbar.BottomAppBar;
+
+import com.example.twitterok.App;
+import com.example.twitterok.databinding.NavDrawerHeaderBinding;
+import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.KeyEvent;
+
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
 import com.example.twitterok.R;
+import com.google.android.material.navigation.NavigationView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,44 +30,56 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_fab_in_bab)
-    public FloatingActionButton floatingActionButton;
+    FloatingActionButton floatingActionButton;
 
-    @BindView(R.id.main_bottom_app_bar)
-    public BottomAppBar bottomAppBar;
+    @BindView(R.id.main_bottom_navigation)
+    BottomNavigationView bottomNavView;
+
+    @BindView(R.id.main_navigation_drawer)
+    NavigationView navigationView;
+
+    @BindView(R.id.main_drawer_layout)
+    DrawerLayout mainDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initBottomAppBar();
+        initNavView();
+        bottomNavView.setOnNavigationItemSelectedListener(selectedListener);
         if (savedInstanceState == null) {
             beginFragment(new MainFragment());
         }
     }
 
-    private void initBottomAppBar() {
-        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu_primary);
-        bottomAppBar.setNavigationOnClickListener(onNavItemClickListener);
-        bottomAppBar.setOnMenuItemClickListener(onMenuItemClickListener);
-    }
-
     @OnClick(R.id.main_fab_in_bab)
     public void clickOnFab(View view) {
-        Intent intent = new Intent(this, NewTweetActivity.class);
-        startActivity(intent);
+        startActivity(NewTweetActivity.class);
     }
 
     @Override
     public void onBackPressed() {
         if (!getSupportFragmentManager().findFragmentById(R.id.main_frame_layout).getTag().equals(MainFragment.class.getSimpleName())) {
-            if (bottomAppBar.getFabAlignmentMode() == BottomAppBar.FAB_ALIGNMENT_MODE_END) {
-                babChangeState(true);
-            }
+            bottomNavView.setSelectedItemId(R.id.bn_home);
             beginFragment(new MainFragment());
         } else {
             super.onBackPressed();
         }
+    }
+
+    public boolean showDrawer(){
+        mainDrawer.openDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void initNavView(){
+        NavDrawerHeaderBinding binding = DataBindingUtil.inflate(getLayoutInflater()
+                ,R.layout.nav_drawer_header
+                ,navigationView,false);
+        navigationView.addHeaderView(binding.getRoot());
+        navigationView.setNavigationItemSelectedListener(navigationViewItemSelectedListener);
+        binding.setUvm(new OwnerViewModel(App.getOwner()));
     }
 
     private void beginFragment(Fragment fragment) {
@@ -66,29 +89,40 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager.executePendingTransactions();
     }
 
-    //true = primary, false = secondary
-
-    private void babChangeState(boolean flag) {
-        bottomAppBar.setFabAlignmentMode(flag ? BottomAppBar.FAB_ALIGNMENT_MODE_CENTER : BottomAppBar.FAB_ALIGNMENT_MODE_END);
-        bottomAppBar.replaceMenu(flag ? R.menu.bottom_app_bar_menu_primary : R.menu.bottom_app_bar_menu_secondary);
-    }
-
-    private BottomAppBar.OnMenuItemClickListener onMenuItemClickListener = item -> {
+    private OnNavigationItemSelectedListener selectedListener = item -> {
         switch (item.getItemId()) {
-            case R.id.bnb_home:
-                babChangeState(true);
+            case R.id.bn_home:
                 beginFragment(new MainFragment());
                 break;
-            case R.id.bnb_search:
-                babChangeState(false);
+            case R.id.bn_search:
                 beginFragment(new SearchFragment());
+                break;
+            case R.id.bn_notification:
+                break;
+            case R.id.bn_messages:
                 break;
         }
         return true;
     };
 
-    private Toolbar.OnClickListener onNavItemClickListener = item -> {
-        BottomNavigationDrawerFragment drawerFragment = new BottomNavigationDrawerFragment();
-        drawerFragment.show(getSupportFragmentManager(), drawerFragment.getTag());
+    private NavigationView.OnNavigationItemSelectedListener navigationViewItemSelectedListener = item -> {
+        switch (item.getItemId()) {
+            case R.id.nv_profile:
+                startActivity(ProfileActivity.class);
+                break;
+            case R.id.nv_lists:
+            case R.id.nv_settings:
+                testToast();
+                break;
+        }
+        return true;
     };
+
+    private void startActivity(Class clazz){
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
+    }
+    private void testToast(){
+        Toast.makeText(this,"clicked",Toast.LENGTH_SHORT).show();
+    }
 }
