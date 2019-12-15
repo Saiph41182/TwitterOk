ff76663523cbd583ba3338ca3b6c401b2bc733e1
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-15 21:19:37 +0300

    Duplicate code moved to base class

--- app/src/main/java/com/example/twitterok/view/HomeFragment.java
+++ app/src/main/java/com/example/twitterok/view/HomeFragment.java
@@ -13,51 +13,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.twitterok.App;
+import com.example.twitterok.BR;
 import com.example.twitterok.R;
 
-import com.example.twitterok.databinding.FragmentMainBinding;
+import com.example.twitterok.databinding.FragmentHomeBinding;
 import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
 import com.example.twitterok.databinding.viewmodel.TimelineViewModel;
+import com.example.twitterok.model.TweetModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
-
-    private TimelineViewModel viewModel;
-
-    public MainFragment() {
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (viewModel != null){
-            viewModel.setUp();
-        }
-    }
+public class HomeFragment extends BaseFragment<TweetModel> {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FragmentMainBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false);
-        View view = binding.getRoot();
-        initRecyclerView(view);
-        viewModel = new TimelineViewModel();
-        binding.setViewModel(viewModel);
-        binding.setOwnerVM(new OwnerViewModel(App.getOwner()));
-        return view;
+        FragmentHomeBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+        initRecyclerView(binding);
+        initViewModel(new TimelineViewModel());
+        initVariable(binding,BR.viewModel,viewModel);
+        initVariable(binding,BR.ownerVM,new OwnerViewModel(App.getOwner()));
+        return binding.getRoot();
     }
-    private void initRecyclerView(View view){
-        RecyclerView recyclerView = view.findViewById(R.id.timeline_recycler_view);
+
+
+    private void initRecyclerView(FragmentHomeBinding binding){
+        RecyclerView recyclerView = binding.timelineRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        if(viewModel != null){
-            viewModel.tearDown();
-        }
-    }
 }
