90e0ed8fd6d3bc1a764f821344d3265e13b8c82e
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-22 16:13:28 +0300

    FAB hiding/showing handler method added

--- app/src/main/java/com/example/twitterok/view/HomeFragment.java
+++ app/src/main/java/com/example/twitterok/view/HomeFragment.java
@@ -1,21 +1,30 @@
 package com.example.twitterok.view;
 
 
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.twitterok.App;
 import com.example.twitterok.BR;
 import com.example.twitterok.R;
 
+import com.example.twitterok.Utils.ClickListenersProvider;
 import com.example.twitterok.databinding.FragmentHomeBinding;
 import com.example.twitterok.databinding.viewmodel.OwnerViewModel;
 import com.example.twitterok.databinding.viewmodel.TimelineViewModel;
@@ -31,12 +40,15 @@ public class HomeFragment extends BaseFragment<TweetModel> {
                              Bundle savedInstanceState) {
         FragmentHomeBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
         initRecyclerView(binding);
+
         initViewModel(new TimelineViewModel());
         initVariable(binding,BR.viewModel,viewModel);
         initVariable(binding,BR.ownerVM,new OwnerViewModel(App.getOwner()));
+
+        handleFabVisibility(binding.timelineRecyclerView);
+
         return binding.getRoot();
     }
-
 
     private void initRecyclerView(FragmentHomeBinding binding){
         RecyclerView recyclerView = binding.timelineRecyclerView;
