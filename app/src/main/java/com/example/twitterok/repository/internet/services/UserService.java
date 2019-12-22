e5535f153821200b654319919f7d75e8140795a7
Michail <38467126+Saiph41182@users.noreply.github.com>
2019-12-22 15:57:50 +0300

    Added method get user by nickname/screen name

--- app/src/main/java/com/example/twitterok/repository/internet/services/UserService.java
+++ app/src/main/java/com/example/twitterok/repository/internet/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.twitterok.repository.internet.services;
 
+import com.twitter.sdk.android.core.models.User;
+
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -7,6 +9,8 @@ import retrofit2.http.Query;
 public interface UserService {
 
     @GET("/1.1/users/show.json")
-    Observable<com.twitter.sdk.android.core.models.User> getUser(@Query("user_id") long id);
+    Observable<User> getUser(@Query("user_id") long id);
 
+    @GET("/1.1/users/show.json")
+    Observable<User> getUser(@Query("screen_name") String screenName);
 }
